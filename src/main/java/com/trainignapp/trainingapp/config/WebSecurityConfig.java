7d8159e96ac745776bc90881957671018f376aee
JOMA <joma.poloza@gmail.com>
2025-01-31 18:22:04 +0400

    Task_SpringSecurity

--- src/main/java/com/trainignapp/trainingapp/config/WebSecurityConfig.java
+++ src/main/java/com/trainignapp/trainingapp/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -48,7 +49,7 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 //enabling cors
                 .cors(Customizer.withDefaults())
 
